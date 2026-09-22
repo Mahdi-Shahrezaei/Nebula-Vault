ac2999a96b60c6b725d8d9b06d1e73c495cc690e
Mahdi <CVANU100@GMAIL.COM>
2026-09-22 19:36:25 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,3 @@
-# Message to my telegram account - > @Mahdi_Shahrezaei 
+# Message to my telegram account - > @AI172   - Contact links in this telegram address
 - > My VIP Personal Security Products View : @AI172
 # Picture in Group Bio
